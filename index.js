c9b227ef64f62cc0a7ab6d5a94978a9791b099d7
marco <marquinco@outlook.com>
2023-01-19 11:49:41 -0300

    finalizado

---
+++ index.js
@@ -0,0 +1,87 @@
+import { create } from 'venom-bot';
+import { Configuration, OpenAIApi } from 'openai';
+
+require('dotenv').config();
+
+const config = new Configuration({
+  apiKey: process.env.OPENAI_KEY,
+  organization: process.env.ORGANIZATION_ID,
+});
+
+const openai = new OpenAIApi(config);
+
+const getDavinciResponse = async (clientText) => {
+  const options = {
+    model: 'text-davinci-003',
+    prompt: clientText,
+    max_tokens: 4000,
+    temperature: 1,
+  };
+
+  try {
+    const response = await openai.createCompletion(options);
+    let botAnswer = '';
+    response.data.choices.forEach(({ text }) => {
+      botAnswer += text;
+    });
+    return `Chat GPT\n\n ${botAnswer.trim()}`;
+  } catch (e) {
+    return `Erro na resposta: ${e.response.data.error.message}`;
+  }
+};
+
+const getDalleResponse = async (clientText) => {
+  const options = {
+    prompt: clientText,
+    n: 1,
+    size: '1024x1024',
+  };
+
+  try {
+    const response = await openai.createImage(options);
+    return response.data.data[0].url;
+  } catch (e) {
+    return `Erro na resposta: ${e.response.data.error.message}`;
+  }
+};
+
+const commands = (client, message) => {
+  const iaCommands = {
+    davinci3: '/bot',
+    dalle: '/img',
+  };
+
+  const firstWord = message.text.substring(0, message.text.indexOf(' '));
+
+  if (firstWord === iaCommands.davinci3) {
+    const question = message.text.substring(message.text.indexOf(' '));
+    getDavinciResponse(question).then((response) => {
+      client.sendText(
+        message.from === process.env.BOT_NUMBER ? message.to : message.from,
+        response,
+      );
+    });
+  }
+  const imgDescription = message.text.substring(message.text.indexOf(' '));
+  getDalleResponse(imgDescription, message).then((imgUrl) => {
+    client.sendImage(
+      message.from === process.env.BOT_NUMBER ? message.to : message.from,
+      imgUrl,
+      imgDescription,
+      'IA DALLE',
+    );
+  });
+};
+
+async function start(client) {
+  client.onAnyMessage((message) => commands(client, message));
+}
+
+create({
+  session: 'Chat-GPT',
+  multidevice: true,
+})
+  .then((client) => start(client))
+  .catch((error) => {
+    console.log(error);
+  });
